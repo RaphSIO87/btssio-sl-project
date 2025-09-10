7b89aaf9186842defc8cecc62fb5eb431856c070
RaphSIO87 <tropini.raphael.sio@gmail.com>
2025-09-10 14:39:03 +0200

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>BTS SIO SL Project</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <h1>Bienvenue sur le projet BTS SIO</h1>
+    <nav>
+        <a href="index.php?page=home">Accueil</a> |
+        <a href="index.php?page=list">Liste</a> |
+        <a href="index.php?page=add">Ajouter</a>
+    </nav>
